fb5d22db2c2d68ba3c847ff2084b721c55eb5a85
Anildare <anilkumardasd85@gmail.com>
2020-06-05 23:39:18 +0530

    added selenium test

---
+++ src/test/java/TestNG/EduWebapp.java
@@ -0,0 +1,32 @@
+package TestNG;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class EduWebapp {
+	  WebDriver driver;
+	@BeforeMethod
+	public void launch() {
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		  driver = new ChromeDriver();
+		  driver.get("http://18.219.57.250:32770/");
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	}
+  @Test
+  public void verifyaddbook() {
+	  driver.findElement(By.xpath("//a[contains(text(),'About Us')]")).click();
+	  
+  }
+  
+  @AfterMethod
+  public void close() {
+	  driver.close();
+	}
+}
